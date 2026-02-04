3982110fd7080a588a32e1c6d096450fe273833b
GayendraSHK <kaveeshagayendra2@gmail.com>
2026-02-04 22:57:26 +0530

    Add Category UI tests for admin and user functionality

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,17 @@ const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor");
 const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+const { webcrypto } = require("crypto");
 
 module.exports = defineConfig({
   e2e: {
     specPattern: "**/*.feature", // Tell Cypress to look for .feature files
     async setupNodeEvents(on, config) {
+      // Polyfill global crypto for @cucumber/* when running under esbuild
+      if (!global.crypto) {
+        global.crypto = webcrypto;
+      }
+
       await addCucumberPreprocessorPlugin(on, config);
       on(
         "file:preprocessor",
