dcc653e3c5dc22b111e70f0ef1c8d4fb5e480aed
sachinthacham <sachinthachamindu26@gmail.com>
2026-02-05 13:49:21 +0530

    Add Cypress steps for  creating sales

---
+++ cypress/e2e/sales/api/SalesApi.js
@@ -0,0 +1,39 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+let plantIdToSell;
+let quantityToSell;
+let initialStock;
+
+Given("I am authenticated as Admin via API", () => {
+ cy.loginAdmin().then(() => {
+    adminToken = Cypress.env("adminToken");
+  });
+});
+
+Given("I am authenticated as a Standard User via API", () => {
+  cy.loginUser().then(() => {
+    userToken = Cypress.env("userToken");
+  });
+});
+
+//ADMIN ACTIONS
+Given("I have a payload for a new sale with Plant ID {int} and Quantity {int}", (pId, qty) => {
+  plantIdToSell = pId;
+  quantityToSell = qty;
+});
+
+When("I send a POST request to {string}", () => {
+  cy.then(() => {
+    const correctUrl = `/api/sales/plant/${plantIdToSell}`;
+    cy.request({
+      method: "POST",
+      url: correctUrl,
+      headers: { 
+        "Authorization": `Bearer ${adminToken}` // Using Admin Token
+      },
+      qs: { quantity: quantityToSell },
+      failOnStatusCode: false
+    }).as('apiResponse');
+  });
+});
+
